878f9dc60ae4adc4e41842c204f5f4bb8c175463
wb-cbg371690 <wb-cbg371690@alibaba-inc.com>
2018-07-03 15:11:01 +0800

    css拆分单独打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path=require('path');
 var webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const common=require('./webpack.common.js');
-const merge = require('webpack-merge');
+const common=require('./webpack.common.js')
+const merge = require('webpack-merge')
 config={
-
   //配置模块的读取和解析规则
   module:{
     rules:[
@@ -14,28 +13,34 @@ config={
       {
         test:[/\.css/,/\.less/],
         //用于单独打包css
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",//style-loader将所有的计算后的样式加入页面中
-          use: [{
-            loader:"css-loader",//css-loader用于支持css的模块化 可以让css支持import require
-            options:{minimize:true,modules:true}
-          },
-          'less-loader',
-          {
-            loader: 'postcss-loader',//css兼容性前缀
-            options: {
-                plugins:  [
-                     require('autoprefixer')({
-                      browsers: ['last 10 versions']
-                  })
-                ]
+        use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: '../'
+              }
+            },
+            {
+              loader:"css-loader",//css-loader用于支持css的模块化 可以让css支持import require
+              options:{minimize:true,modules:true}
+            },
+            'less-loader',
+            {
+              loader: 'postcss-loader',//css兼容性前缀
+              options: {
+                  plugins:  [
+                       require('autoprefixer')({
+                        browsers: ['last 10 versions']
+                    })
+                  ]
+              }
             }
-          }
+          ],
+          exclude:[
+            'node_module','dist'
           ]
-        }),
-        exclude:[
-          'node_module','dist'
-        ]
       },
 
     ]
@@ -45,11 +50,16 @@ config={
   plugins:[
     new CleanWebpackPlugin(['dist']),//每次编译 清空dist文件夹
     //代码压缩 & 删除不被引用的多余代码  在webpack进行摇树优化之后吧bundle中没有被外部引用的代码删除
-    new UglifyJSPlugin(),
+    //从webpack4起 指定环境为生产环境会自动调用UglifyJSPlugin()；
+    // new UglifyJSPlugin(),
     //指定环境为生产环境 一些 library 可能针对具体用户的环境进行代码优化，从而删除或添加一些重要代码
     new webpack.DefinePlugin({
        'process.env.NODE_ENV': JSON.stringify('production')
-     })
+     }),
+     new MiniCssExtractPlugin({
+   　　filename: "[name].[chunkhash:8].css",
+  　　 chunkFilename: "[id].css"
+　　 })
   ],
 
   //代码调试映射模式 (map文件) 用于追踪调试报错和源码位置
