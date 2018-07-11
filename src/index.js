1dd1c84087e9397160de102c082087a15324b2fd
CBGhaha <875675253@qq.com>
2018-07-11 19:05:24 +0800

    add react-redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 import MyRouter from './router/router.jsx';
@@ -7,6 +9,7 @@ import styles from './global.less'
 //index入口文件并不会在整个架构体系有变化时自动重新执行（会重新打包）
 //比如console.log(a)这个语句 在启动后执行一次后 当我修改a.js后重新打包 但这句语句是不会重新执行的 除非刷新
 //所以热模块替换（HMR）使用module.hot.accept监听变化 并触发变化后执行的操作（重新）
+//AppContainer 是react基于webpack的HMR的loader 它使得热加载后一些之前编译的旧内容(某个已经渲染的dom，某个dom上的旧事件)获得更新
 renderWithHotReload(MyRouter());
 if(module.hot){
   module.hot.accept('./router/router.jsx',()=>{
@@ -17,9 +20,11 @@ if(module.hot){
 function renderWithHotReload(element){
   ReactDOM.render(
           <AppContainer>
-  	        <div>
-              {element}
-  	         </div>
+            <Provider store={store}>
+              <div>
+                {element}
+              </div>
+            </Provider>
           </AppContainer>,
           document.getElementById('appContent')
       )
